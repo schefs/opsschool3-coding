fa9b07d484d9ea4510df1244ef8d0b2a9d558f8f
unknown <eyalschefler@gmail.com>
2018-11-07 01:23:20 +0200

    Add exercise1

---
+++ home-assignments/session1/exercise1.py
@@ -0,0 +1,40 @@
+import json
+import yaml
+JSON_FILE_NAME = "my_list.json"
+
+
+def read_json_file(file):
+    with open(file) as f:
+        data = json.load(f)
+    return data
+
+
+def main():
+    final_dict = {}
+    my_json = read_json_file(JSON_FILE_NAME)
+    buckets = my_json["buckets"]
+    buckets.sort()
+    ppl_ages = my_json["ppl_ages"]
+
+    # Starting final dict with empty arries
+    final_dict[str(buckets[0])] = []
+    for i in range(len(buckets) - 1):
+        final_dict[str(buckets[i]) + "-" + str(buckets[i + 1])] = []
+
+    for ppl in ppl_ages:
+        age = ppl_ages[ppl]
+        if age < buckets[0]:
+            final_dict[str(buckets[0])].append(ppl)
+        else:
+            for i in range(len(buckets) - 1):
+                if buckets[i] <= age < buckets[i+1]:
+                    final_dict[str(buckets[i]) + "-" + str(buckets[i+1])].append(ppl)
+                    break
+
+    with open(JSON_FILE_NAME.replace(".json", ".yml"), "w") as f:
+        yaml.dump(final_dict, f, default_flow_style=False, allow_unicode=True)
+    print(final_dict)
+
+
+if __name__ == '__main__':
+    main()
